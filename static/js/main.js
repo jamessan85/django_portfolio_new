c5dc2c34cfc64616b2d75987e5e608340d0e1901
James Sanderson <jamessandersoon@gmail.com>
2017-09-10 16:34:03 +0100

    near completion

--- static/js/main.js
+++ static/js/main.js
@@ -1,16 +1,29 @@
 $( document ).ready(function() {
-    $('#portfolio').click(function() {
-        $('.body-container1').slideToggle(1000);
-    })
-    $('.upbutton').click(function() {
-         $('.body-container1').slideUp(1000);
-    })
-    $('.sendbutton').mouseover(function() {
-        $(this).addClass("Animate");
-        $(this).css({'border':'black 1px solid','color':'#6b6d70','background-color':'black'});
-    })
-    $('.sendbutton').mouseleave(function() {
-      $(this).removeClass("Animate",1000);
-      $(this).css({'border':'none','color':'#6b6d70','background-color':'#dde0db'});
-    })
+    var windowWidth = $(window).width();
+    if(windowWidth < 600){
+      $('#portfolio').click(function() {
+          $('.body-container').css("margin-top", "10px")
+          $('.body-container2').slideToggle(1000)
+      })
+      $('.upbutton').click(function() {
+          $('.body-container').css("margin-top", "130px")
+           $('.body-container2').slideUp(1000);
+      })
+    }
+    if (windowWidth >= 601 ){
+      $('#portfolio').click(function() {
+          $('.body-container1').slideToggle(1000);
+      })
+      $('.upbutton').click(function() {
+           $('.body-container1').slideUp(1000);
+      })
+    }
+
+
+  $('.sendbutton').mouseover(function() {
+      $(this).css({'border':'#363531 4px solid','color':'#6b6d70','background-color':'black'});
+  })
+  $('.sendbutton').mouseleave(function() {
+    $(this).css({'border':'#363531 4px solid','color':'#6b6d70','background-color':'#dde0db'});
+  })
 })
