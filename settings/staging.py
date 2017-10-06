e43e137a2b6a6af4eab39e4f4e946055134ee3f8
James Sanderson <jamessandersoon@gmail.com>
2017-10-06 16:05:07 +0100

    debig false

--- settings/staging.py
+++ settings/staging.py
@@ -4,8 +4,8 @@ from base import *
 
 import dj_database_url
 
-DEBUG = True
- 
+DEBUG = False
+
 # Load the ClearDB connection details from the environment variable
 DATABASES = {
     'default': dj_database_url.config('CLEARDB_DATABASE_URL')
