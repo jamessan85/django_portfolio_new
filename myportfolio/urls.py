861b002ee9d59331d9f5f1631ce8947e9b68e231
James Sanderson <jamessandersoon@gmail.com>
2017-10-15 21:48:55 +0100

    change debug

--- myportfolio/urls.py
+++ myportfolio/urls.py
@@ -17,8 +17,8 @@ from django.conf.urls import url
 from django.contrib import admin
 from projects import views
 from django.views.static import serve
-from settings import MEDIA_ROOT
-#from settings.base import MEDIA_ROOT
+#from settings import MEDIA_ROOT
+from settings.base import MEDIA_ROOT
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
