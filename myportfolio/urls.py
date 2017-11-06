4037e7fc6a0f74a6d040b6831a6ea1ed3b59b529
James Sanderson <jamessandersoon@gmail.com>
2017-11-06 19:59:26 +0000

    changed field length

--- myportfolio/urls.py
+++ myportfolio/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
     url(r'^projects/(?P<id>\w+)/$', views.product_detail, name='projects'),
     url(r'^aboutme/$', views.about_me, name='aboutme'),
     url(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT}),
-    # url(r'^contact/$', views.contact, name='contact'),
+    #url(r'^contact/$', views.contact, name='contact'),
 ]
--- projects/models.py
+++ projects/models.py
@@ -6,7 +6,7 @@ from django.db import models
 # Create your models here.
 class Project_Info(models.Model):
     project_name = (models.CharField(max_length=40, null=True))
-    project_description = (models.TextField(max_length=200, null=True))
+    project_description = (models.TextField(max_length=1000, null=True))
     project_image = (models.ImageField(upload_to="images", null=True))
     project_image_high_res = (models.ImageField(upload_to="images", null=True))
     project_git = (models.CharField(max_length=100, null=True))
